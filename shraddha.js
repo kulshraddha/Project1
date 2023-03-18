aa57306f6f89d0c24958f851f2a56755e3d8be3b
shraddha <alatshraddha01@gmail.com>
2023-03-18 20:12:01 +0530

    second change

--- shraddha.js
+++ shraddha.js
@@ -1,1 +1,10 @@
-<div>shraddha</div>
+import React from 'react';
+const shraddha = () => {
+    return ( 
+        <>
+        <h2>shraddha</h2>
+        </>
+     );
+}
+ 
+export default shraddha;
